df6b2afda3d72280a03e525a41d9da8955337f88
austinbeaufort <austinbeaufort7@gmail.com>
2020-05-24 17:51:25 -0400

    testing deployment

---
+++ src/domIds.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+var form = document.querySelector('#form');
+exports.form = form;
+var message = document.querySelector('#message');
+exports.message = message;
